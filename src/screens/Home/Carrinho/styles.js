269b6d60bbbe3a1a0a6b9dad6f6709417c0e01bb
Isaque Costa <isaquecostaa@gmail.com>
2020-04-29 17:32:44 -0300

    before redux '

--- src/screens/Home/Carrinho/styles.js
+++ src/screens/Home/Carrinho/styles.js
@@ -1,93 +1,92 @@
 import commonStyles from '~/assets/styles/commonStyles';
 
 export default {
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    tinyIcon: {
-        width: 25,
-        height: 25,
-    },
-    
-    infoWrapper: {
-        width: '70%',
-        marginVertical: 10,
-        alignSelf: 'flex-end',
-        alignContent: 'center',
-    },
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  tinyIcon: {
+    width: 25,
+    height: 25,
+  },
 
-    infoContent: {
-        marginHorizontal: 5
-    },
+  infoWrapper: {
+    width: '70%',
+    marginVertical: 10,
+    alignSelf: 'flex-end',
+    alignContent: 'center',
+  },
 
-    infoHeader: {
-        width: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        alignContent: 'center',
-        justifyContent: 'space-between',
-    },
+  infoContent: {
+    marginHorizontal: 5,
+  },
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  infoHeader: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'space-between',
+  },
 
-    itemWrapper: {
-        marginBottom: 10,
-        flexDirection: 'row',
-        alignItems: 'center',
-        alignContent: 'center',
-        justifyContent: 'space-between',
-    },
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    itemContent: {
-        flex: 1,
-        marginHorizontal: 20
-    },
+  itemWrapper: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'space-between',
+  },
 
-    itemHeader: {
-        width: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        alignContent: 'center',
-        justifyContent: 'space-between',
-    },
-    itemName: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        color: commonStyles.colors.black,
-    },
-    itemPrice: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: commonStyles.colors.success,
-    },
-    itemFooter: {
-        fontSize: 15,
-        color: commonStyles.colors.black,
-    },
+  itemContent: {
+    flex: 1,
+    marginHorizontal: 20,
+  },
 
+  itemHeader: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'space-between',
+  },
+  itemName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: commonStyles.colors.black,
+  },
+  itemPrice: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: commonStyles.colors.success,
+  },
+  itemFooter: {
+    fontSize: 15,
+    color: commonStyles.colors.black,
+  },
 
-    totalWrapper: {
-        marginTop: 10,
-        marginBottom: 35,
-    },
-    totalHeader: {
-        marginHorizontal: 5,
-        flexDirection: 'row',
-        alignItems: 'center',
-        alignContent: 'center',
-        justifyContent: 'space-between',
-    },
-    totalTitle: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginRight: 5,
-        color: commonStyles.colors.black,
-    },
-    totalPrice: {
-        fontSize: 23,
-        fontWeight: 'bold',
-        color: commonStyles.colors.black,
-    },
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-}
+  totalWrapper: {
+    marginTop: 10,
+    marginBottom: 35,
+  },
+  totalHeader: {
+    marginHorizontal: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'space-between',
+  },
+  totalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginRight: 5,
+    color: commonStyles.colors.black,
+  },
+  totalPrice: {
+    fontSize: 23,
+    fontWeight: 'bold',
+    color: commonStyles.colors.black,
+  },
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+};
