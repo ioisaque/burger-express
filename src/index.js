5d01ea270e98db04ffc8059784054cbbe27f7265
Carlos Roberto <carlosrpj555@gmail.com>
2020-04-29 06:38:42 -0300

    persist store change, boilerplate

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
 import React from 'react';
-import '~/config/ReactotronConfig';
-
-import {PersistGate} from 'redux-persist/integration/react';
-import {Provider} from 'react-redux';
-import {StatusBar, YellowBox} from 'react-native';
-
-// import {store, persistor} from '~/store';
-import Routes from '~/routes';
-import commonStyles from './assets/styles/commonStyles';
-import {AppContainer} from './components/styledComponents';
+import {YellowBox} from 'react-native';
+import App from './App';
 
 YellowBox.ignoreWarnings(['VirtualizedLists', 'forwardRef']);
 
-const App = () => (
-  // <Provider store={store}>
-  // <PersistGate persistor={persistor}>
-  <AppContainer>
-    <StatusBar
-      barStyle="dark-content"
-      backgroundColor={commonStyles.colors.gold}
-    />
-    <Routes />
-  </AppContainer>
-  // </PersistGate>
-  // </Provider>
-);
-
-export default App;
+export default () => <App />;
--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -1,5 +1,4 @@
 import AsyncStorage from '@react-native-community/async-storage';
-import {storage} from 'redux-persist/lib/storage';
 import {persistReducer} from 'redux-persist';
 
 export default reducers => {
