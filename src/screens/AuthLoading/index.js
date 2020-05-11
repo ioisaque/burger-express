4dea6d33581e7e35660b5e68f98333d3b4038ef3
Carlos Roberto <carlos@ideyou.com.br>
2020-05-11 00:58:46 -0300

    start of auth loading screen

---
+++ src/screens/AuthLoading/index.js
@@ -0,0 +1,21 @@
+import React, {useEffect} from 'react';
+import {View} from 'react-native';
+import auth from '@react-native-firebase/auth';
+
+// import { Container } from './styles';
+
+export default function AuthLoading(props) {
+  const {navigation} = props;
+
+  useEffect(() => {
+    auth().onAuthStateChanged(user => {
+      if (user) {
+        navigation.navigate('App');
+      } else {
+        navigation.navigate('Login');
+      }
+    });
+  }, []);
+
+  return <View />;
+}
